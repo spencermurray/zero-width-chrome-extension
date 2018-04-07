7b758ca33ccdb6a80c468e64d2b02cca553c013a
spencermurray <spencermurray@gmail.com>
2018-04-06 20:23:55 -0400

    update background script to update the badge icon per tab

--- background.js
+++ background.js
@@ -1,7 +1,24 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   switch(message.type) {
     case "updateBadge":
-      chrome.browserAction.setBadgeText({text: '' + message.text})
+    console.log(sender)
+      chrome.tabs.get(sender.tab.id, tab => {
+        // prerendered tab has been destroyed
+        if (chrome.runtime.lastError) return;
+        // tab is visible
+        if (tab.index >= 0) {
+          chrome.browserAction.setBadgeText({tabId: tab.id, text: '' + message.text});
+        } else {
+          // handle a prerendered tab that's not visible
+          const tabId = sender.tab.id, text = message.text
+          chrome.webNavigation.onCommitted.addListener(function update(details){
+            if (details.tabId === tabId) {
+              chrome.browserAction.setBadgeText({tabId: tabId, text: '' + text});
+              chrome.webNavigation.onCommitted.removeListener(update);
+            }
+          });
+        }
+      })
       break;
     default:
       console.warn('Unrecognized message type received')
